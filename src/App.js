26b24b0d3a8b8f5dbb53eb1741a7cb52c661868e
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2022-03-15 15:51:37 +0600

    added html file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Hulk Bluster",
+    job:"Smash"
+
+  }
+  var person2 = {
+    name: "Captain America",
+    job: "Dhal"
+  }
+  var style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>My first react app</p>
+        <h1 className='' style={style}>Hello hulk: {person.name + " " + person.job}</h1>
+        <h2 style = {{backgroundColor:'cyan', color:'yellow'}}>Hello cap: {person2.name + " " + person2.job}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
