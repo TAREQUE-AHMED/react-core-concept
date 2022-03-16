2093ed85a996732d1bf675713d8bbeb8312ea97e
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2022-03-16 15:07:49 +0600

    added code1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 function App() {
   const players = ['Smith', 'Lara', 'Tendulkar', 'Shangakara'];
+  const products = [{
+    name:'Photoshop', price:'$90.99'
+  },
+    { name: 'Adobe', price: '$60.98' },
+    {name:'PDF Reader', proce: '$10.52'}
+  ]
   var person = {
     name: "Hulk Bluster",
     job:"Smash"
@@ -35,11 +41,35 @@ function App() {
         >
           Learn React
         </a>
+        <Product name ={products[0].name}></Product>
         <Person name = {players[2]} job="smash"></Person>
         <Person></Person>
+        <Friend name = 'Salman' nayika = 'Sabnur'></Friend>
+        <Friend name = 'Hulk' nayika = 'Jeli'></Friend>
+        <h5>Component</h5>
+        <Person4 name = 'Jack Kallis' job='Actor'></Person4>
+        <Person4 name = 'Captain' job= 'Actor'></Person4>
+        <Person4 name = 'Thor' job='Thandar'></Person4>
+        
       </header>
     </div>
   );
+  function Product(props) {
+    const ProductStyle = {
+      border: '1px solid gray',
+      backgroundColor: 'lightgray',
+      borderRadius: '5px',
+      height: '200px',
+      width: '200px',
+      float: 'left'
+    }
+    return (
+      <div>
+        <h2>{props.name}</h2>
+      
+      </div>
+    )
+  }
   function Person(props) {
     const personStyle = {
       border: '2px solid yellow',
@@ -52,6 +82,22 @@ function App() {
         <h4>Job: {props.job}</h4>
       
       </div>
+    )
+  };
+  function Friend (props) {
+    return (
+      <div className="friend">
+        <h1>Name:{props.name} </h1>
+        <p>Nayika: {props.nayika} </p>
+      </div>
+    )
+  }
+  function Person4(props) {
+    return (
+      <div className="person4">
+        <h1>Name: {props.name}</h1>
+        <p>Job: {props.job}</p>
+     </div>
     )
   }
 }
